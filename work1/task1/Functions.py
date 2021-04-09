a51ff31ecb963f09f0dc2fb86274d7f7e29d05a0
nimrodberman <33838639+nimrodberman@users.noreply.github.com>
2021-04-09 16:49:43 +0300

    Add files via upload

--- work1/task1/Functions.py
+++ work1/task1/Functions.py
@@ -32,27 +32,68 @@ def accuracy(prob, testSetY):
 
 
 class NN:
-    def __init__(self, inputSize, outputSize, layerNumber):
+    def __init__(self, inputSize, outputSize, hiddenSize, layerNumber):
         # model parameters
         self.inputSize = inputSize
         self.outputSize = outputSize
+        self.hiddenSize = hiddenSize
         self.layerNumber = layerNumber
         # model architecture
-        self.initParameters(inputSize,outputSize layerNumber)
-        # self.wOutput = nn.Linear(hiddenStateSize,outputSize, bias=True)
-        # self.rnn = LvfModel(inputSize, hiddenStateSize)
-        # self.lossFunction = nn.CrossEntropyLoss()
+        self.initParameters(inputSize, outputSize, hiddenSize, layerNumber)
+        self.weightsArray = []
+        self.biasArray = []
 
-    def initParameters(self ,inputSize, outputSize, layerNumber):
-        retrun 0;
+    def initParameters(self):
+        # set the input layer
+        self.theta_in = np.random.rand(self.inputSize, self.hiddenSize)
+        self.bias_in = np.zeros([1, self.hiddenSize])
 
-    def lossFunction(self):
-        return 0;
+        # set hidden layers by the layer number parameters
+        for i in range(self.layerNumber - 2):
+            theta = np.random.rand(self.hiddenSize, self.hiddenSize)
+            bias = np.zeros([1, self.hiddenSize])
+            self.weightsArray.append(theta)
+            self.biasArray.append(bias)
 
-    def forward(self):
-        return 0;
+        # set the output layer
+        self.theta_out = np.random.rand(self.hiddenSize, self.outputSize)
+        self.bias_out = np.zeros([1, self.outputSize])
 
-    def backprop(self):
+    def lossFunction(self, probs, target):
+        # return log loss average
+        return (-1 / probs.shape[1]) * (np.sum(target.T * np.log(probs)))
+
+    def forward(self, X):
+        layerOutputs = []
+
+        # input layer forward with tanh
+        X_1 = np.tanh(np.dot(np.transpose(X), self.theta_in) + self.bias_in)
+        layerOutputs.append(X_1)
+
+        X_L = X_1.copy()
+        # hidden layer forward with tanh
+        for w, b in zip(self.weightsArray, self.biasArray):
+            X_L = np.tanh(np.dot(np.transpose(X_L), w) + b)
+            layerOutputs.append(X_L)
+
+        # softmax output layer
+        X_out = np.dot(np.transpose(X_L), self.theta_out) + self.bias_out
+        X_prob = softmax(X_out)
+        layerOutputs.append(X_prob)
+
+        return X_prob
+
+    def backprop(self, probs, target, x_l, lr):
+        gradArray = []
+        # softmax output layer gradients
+        out_grad_theta = (-1 / x_l.shape[1]) * (x_l @ (target.T - probs))
+        out_grad_b = -(1 / x_l.shape[1]) * np.sum(target.T - probs, axis=0).T
+        gradArray.append(out_grad_theta)
+        gradArray.append(out_grad_b)
+
+        # other tanh layers gradients
+
+
         return 0;
 
     def accuracy(self):
