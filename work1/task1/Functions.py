fixed dimensions, but the gradient explodes during backpropagation

work1/task1/Functions.py
@@ -1,6 +1,6 @@
 import numpy as np
-from networkx.algorithms.tests.test_communicability import scipy
-
+# from networkx.algorithms.tests.test_communicability import scipy
+import math
 
 def softmaxRegression(theta_L, x_L, b_L, y_mat):
     # active softmax
@@ -32,35 +32,43 @@ def accuracy(prob, testSetY):
 
 
 class NN:
-    def __init__(self, inputSize, outputSize, hiddenSize, layerNumber):
+    def __init__(self, inputSize, outputSize, hiddenSize, hiddenLayerAmount):
         # model parameters
         self.inputSize = inputSize
         self.outputSize = outputSize
         self.hiddenSize = hiddenSize
-        self.layerNumber = layerNumber
+        self.hiddenLayerAmount = hiddenLayerAmount
         # model architecture
+        # weight array will contain the input layer and the hidden layers
         self.weightsArray = []
         self.biasArray = []
         self.initParameters()
 
     def initParameters(self):
+        # collect the weights with normal distribution
+        np.random.seed(0)
+        scale = 1 / max(1., (2 + 2) / 2.)
+        limit = math.sqrt(3.0 * scale)
         # set the input layer
-        self.theta_in = np.random.rand(self.inputSize, self.hiddenSize)
+        theta_in = np.random.uniform(-limit, limit, size=(self.inputSize, self.hiddenSize))
+        # self.theta_in = np.random.randn(self.inputSize, self.hiddenSize)
         self.bias_in = np.zeros([1, self.hiddenSize])
-        self.weightsArray.append(self.theta_in)
+        self.weightsArray.append(theta_in)
         self.biasArray.append(self.bias_in)
 
         # set hidden layers by the layer number parameters
-        for i in range(self.layerNumber - 2):
-            theta = np.random.rand(self.hiddenSize, self.hiddenSize)
+        for i in range(self.hiddenLayerAmount-1):
+            theta = np.random.uniform(-limit, limit, size=(self.hiddenSize, self.hiddenSize))
+            # theta = np.random.rand(self.hiddenSize, self.hiddenSize)
             bias = np.zeros([1, self.hiddenSize])
             self.weightsArray.append(theta)
             self.biasArray.append(bias)
 
         # set the output layer
-        self.theta_out = np.random.rand(self.hiddenSize, self.outputSize)
+        self.theta_out=np.random.uniform(-limit, limit, size=(self.hiddenSize, self.outputSize))
+        # self.theta_out = np.random.rand(self.hiddenSize, self.outputSize)
         self.bias_out = np.zeros([1, self.outputSize])
-
+        # the weightArray doesnt contain the output layer because we use different activation function
     def lossFunction(self, probs, target):
         # return log loss average
         grad = -(1) * target.T - probs
@@ -74,6 +82,7 @@ class NN:
         X_L = X.copy()
         # hidden layer forward with tanh
         for w, b in zip(self.weightsArray, self.biasArray):
+            # print(f"XL={X_L.shape}    w={w.shape}       b={b.shape} ")
             X_L = np.tanh(np.dot(np.transpose(X_L), w) + b).T
             layerOutputs.append(X_L)
 
@@ -87,27 +96,42 @@ class NN:
     def backprop(self, lr, dl_dy, layerOutputs):
         theta_grads = []
         bias_grads = []
-        # softmax output layer gradients
-        dl_dhy = layerOutputs[-1] @ dl_dy
-        dl_dby = dl_dy
-        theta_grads.append(dl_dhy)
-        bias_grads.append(dl_dby)
-        dl_dh_curr = self.theta_out @ dl_dy.T
-        # running over the layers not include the last one
-        for layer, t in zip(reversed(layerOutputs[:-1]), reversed(range(len(layerOutputs) - 1))):
-            temp = ((1 - layer ** 2) * dl_dh_curr)
-            bias_grads.append(temp)
-            theta_grads.append(temp @ layerOutputs[t].T)
-            if t > 0:
-                dl_dh_curr = self.weightsArray[t-1] @ temp
 
+        dl_dby = dl_dy
+        # softmax output layer gradients, the gradient of the last layer is
+        # dl/dy * last_hidden_layer_values
+        theta_out_grad = layerOutputs[-1] @ dl_dy
+        bias_out_grad = dl_dby
+
+        dl_dh_curr = self.theta_out @ dl_dy.T
+        # running over the hidden layers
+        for t in reversed(range(self.hiddenLayerAmount)):
+            temp = ((1 - layerOutputs[t+1] ** 2) * dl_dh_curr)
+            theta_grads.append(-temp @ layerOutputs[t].T)
+            bias_grads.append(-temp)
+            dl_dh_curr = self.weightsArray[t] @ temp
+        # for layer, t in zip(reversed(layerOutputs[:-1]), reversed(range(len(layerOutputs) - 1))):
+        #     temp = ((1 - layer ** 2) * dl_dh_curr)
+        #     bias_grads.append(temp)
+        #     theta_grads.append(temp @ layerOutputs[t-1].T)
+        #     if t > 0:
+        #         dl_dh_curr = self.weightsArray[t-1] @ temp
+
+        # temp = ((1 - layerOutputs[1] ** 2) * dl_dh_curr)
+        # theta_in_grad = temp @ layerOutputs[0].T
+        # bias_in_grad = temp
         bias_grads = list(reversed(bias_grads))
         theta_grads = list(reversed(theta_grads))
 
-        for index in range(len(bias_grads)):
-            if index <= len(bias_grads) - 2:
-                self.weightsArray[index] = self.weightsArray[index] - theta_grads[index].T * lr
-                self.biasArray[index] = self.biasArray[index] - bias_grads[index].T * lr
-            else:
-                self.theta_out = self.theta_out - dl_dhy.T * lr
-                self.bias_out = self.bias_out - dl_dby.T * lr
+        # update wights
+        # self.theta_in = self.theta_in - theta_in_grad.T*lr
+        # self.bias_in = self.bias_in - np.mean(bias_in_grad ,axis=1).T*lr
+        for index in range(self.hiddenLayerAmount):
+            self.weightsArray[index] = self.weightsArray[index] - theta_grads[index].T * lr
+            self.biasArray[index] = self.biasArray[index] - np.mean(bias_grads[index].T ,axis=0)* lr
+            # self.weightsArray[index] = np.clip(self.weightsArray[index] - theta_grads[index].T * lr, -1, 1, self.weightsArray[index])
+            # self.biasArray[index] = np.clip((self.biasArray[index] - np.mean(bias_grads[index].T, axis=0) * lr), -1, 1, self.biasArray[index])
+        self.theta_out = self.theta_out - theta_out_grad * lr
+        self.bias_out = self.bias_out - np.mean(bias_out_grad ,axis=0) * lr
+        # self.theta_out = np.clip(self.theta_out - theta_out_grad * lr, -1, 1,self.theta_out)
+        # self.bias_out = np.clip(self.bias_out - np.mean(bias_out_grad ,axis=0) * lr, -1, 1,self.bias_out)

work1/task1/GradientTest.py
@@ -43,29 +43,6 @@ def WeightsGradientTest(theta, b, xs, ys):
     plt.show()
 
 
-# def gradientTest1(x,theta, bias, y_mat):
-#     # sample a random unit vector
-#     d = np.random.rand(x.shape[0])
-#     d = d / np.linalg.norm(d)
-#
-#     firstOrderArr = []
-#     secondOrderArr = []
-#
-#     cost, grad_theta, grad_b, _ = softmaxRegression(theta, x, bias,y_mat)
-#
-#     for eps in epsilonArray:
-#         theta_diff = theta
-#         theta_diff = theta_diff + (d * eps).reshape(2, 1)
-#         epsilon_cost = softmaxRegression(theta_diff, x, bias, y_mat)[0]
-#
-#         firstOrderArr.append(abs(epsilon_cost - cost))
-#         secondOrderArr.append(abs(epsilon_cost - cost - (eps * (d.T @ grad_theta))))
-#
-#     plt.plot(firstOrderArr, epsilonArray, secondOrderArr, epsilonArray)
-#     print("finished test 1")
-#     return 0;
-
-
 def BiasGradientTest(X, Y, W, b):
     iter = 20
     diff = np.zeros(iter)
@@ -90,3 +67,6 @@ def BiasGradientTest(X, Y, W, b):
     plt.title('bais gradient test result')
     plt.legend('diff without grad', 'diff with grad')
     plt.show()
+
+
+# def jacobian_test_input(X,thetha):

work1/task1/NNmain.py
@@ -7,14 +7,19 @@ if __name__ == '__main__':
     print("main started")
 
     # upload data and set parameters for it
-    sample_size = 20
+    number_of_butches = 3000
+    testSetSize = 200
+    iterations = 1000
+    lr = 0.0001
+    np.random.seed(0)
+
+
     PeaksData = loadmat('PeaksData.mat')
     trainSet = np.array(PeaksData['Yt'])
     trainSetLabels = np.array(PeaksData['Ct'])
 
     validationSet = np.array(PeaksData['Yv'])
     validationSetLabels = np.array(PeaksData['Cv'])
-    testSetSize = 200
 
     #  shuffle validation set
     idx = np.random.permutation(len(validationSetLabels[0]))
@@ -27,8 +32,8 @@ if __name__ == '__main__':
     trainSetY = trainSetLabels[:, idx]
 
     # split into batches
-    trainSetX_batches = np.array_split(trainSetX, 3000, axis=1)
-    trainSetY_batches = np.array_split(trainSetY, 3000, axis=1)
+    trainSetX_batches = np.array_split(trainSetX, number_of_butches, axis=1)
+    trainSetY_batches = np.array_split(trainSetY, number_of_butches, axis=1)
 
     # set parameters
     output_size = len(trainSetY_batches[0][:, 0])
@@ -37,15 +42,15 @@ if __name__ == '__main__':
     # gradient test
     # WeightsGradientTest(theta,bias,trainSetX_batches[0],trainSetY_batches[0])
 
-    nn_model = Functions.NN(input_size, output_size, hiddenSize=6, layerNumber=4)
+    nn_model = Functions.NN(input_size, output_size, hiddenSize=6, hiddenLayerAmount=2)
 
-    iterations = 1000
-    lr = 0.0001
+
     for i in range(0, iterations):
-        for batchX, batchY in zip(trainSetX_batches[0:20], trainSetY_batches[0:20]):
+        for batchX, batchY in zip(trainSetX_batches, trainSetY_batches):
             X_prob, layerOutputs = nn_model.forward(batchX)
             loss, dl_dy = nn_model.lossFunction(probs=X_prob, target=batchY)
             nn_model.backprop(lr=lr, dl_dy=dl_dy, layerOutputs=layerOutputs)
         if i % 10 == 0:
-            acc = accuracy(X_prob, testSetY)
+            X_prob, layerOutputs = nn_model.forward(trainSetX)
+            acc = accuracy(X_prob, trainSetY)
             print(f"iter number:{i}/{iterations}   loss: {loss}    accuracy:{acc}%")

work1/task1/main.py
@@ -1,13 +1,20 @@
 import numpy as np
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+
 from work1.task1 import Functions
 from work1.task1.GradientTest import *
-from scipy.io import loadmat
-
 if __name__ == '__main__':
     print("main started")
 
     # upload data and set parameters for it
-    sample_size = 20
+    # sample_size = 20
+
+    number_of_butches = 200
+    learningRate = 0.0001
+    iterations = 800
+
+
     PeaksData = loadmat('PeaksData.mat')
     trainSet = np.array(PeaksData['Yt'])
     trainSetLabels = np.array(PeaksData['Ct'])
@@ -27,8 +34,8 @@ if __name__ == '__main__':
     trainSetY = trainSetLabels[:, idx]
 
     # split into batches
-    trainSetX_batches = np.array_split(trainSetX, 3000, axis=1)
-    trainSetY_batches = np.array_split(trainSetY, 3000, axis=1)
+    trainSetX_batches = np.array_split(trainSetX, number_of_butches, axis=1)
+    trainSetY_batches = np.array_split(trainSetY, number_of_butches, axis=1)
 
     # set parameters
     output_size = len(trainSetY_batches[0][:, 0])
@@ -37,18 +44,47 @@ if __name__ == '__main__':
     bias = np.zeros([1, output_size])
 
     # gradient test
-    WeightsGradientTest(theta,bias,trainSetX_batches[0],trainSetY_batches[0])
+    # WeightsGradientTest(theta,bias,trainSetX_batches[0],trainSetY_batches[0])
 
-
-    iterations = 1000
-    learningRate = 0.0001
+    train_accuracys=[]
+    test_accuracys=[]
+    losses=[]
     for i in range(0, iterations):
         for batchX, batchY in zip(trainSetX_batches, trainSetY_batches):
-            loss, grad_theta, grad_b, _ = softmaxRegression(theta, batchX, bias, batchY)
+            loss, grad_theta, grad_b, pred = softmaxRegression(theta, batchX, bias, batchY)
             theta = theta - (learningRate * grad_theta)
             bias = bias - (learningRate * grad_b)
-
+        # check current accuracy and store it
+        loss, _, _, pred = softmaxRegression(theta, trainSetX, bias, trainSetY)
+        acc_train = accuracy(pred, trainSetY)
+        _, _, _, pred = softmaxRegression(theta, testSetX, bias, testSetY)
+        acc_test = accuracy(pred, testSetY)
+        train_accuracys.append(acc_train)
+        test_accuracys.append(acc_test)
+        losses.append(loss)
         if i % 10 == 0:
-            _, _, _, pred = softmaxRegression(theta, testSetX, bias, testSetY)
-            acc = accuracy(pred, testSetY)
-            print(f"iter number:{i}/{iterations}   loss: {loss}    accuracy:{acc}%")
+            print(f"iter number:{i}/{iterations}   loss: {loss}    accuracy:{acc_test}%    batch size:{batchX.shape[1]}")
+
+    xbar=np.arange(len(train_accuracys))
+    # plt.plot(xbar, train_accuracys, label="train")
+    # plt.plot(xbar, test_accuracys, label="test")
+    #
+    # # plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='center',
+    # #            ncol=2, mode="expand", borderaxespad=0.)
+    # plt.xlabel('epoch')
+    # plt.ylabel('accuracy')
+    # plt.title('Success Percentages')
+    # plt.legend('train', 'test')
+    # plt.show()
+
+    fig, ax = plt.subplots()
+    ax.plot(xbar, train_accuracys,color='blue', label='train')
+    ax.plot(xbar, test_accuracys, color='red', label='test')
+    # ax.plot(xbar, losses, 'k:', label='loss')
+
+    legend = ax.legend(loc='lower right', fontsize='x-large')
+    legend.get_frame().set_facecolor('C0')
+    plt.title('Success Percentages')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.show()