a9000f1857e220568606a23ce3f8807ade969e96
nimrod berman <33838639+nimrodberman@users.noreply.github.com>
2021-05-14 14:36:42 +0300

    work2 3.1 - cross validation, reconstruction compare and test set

--- work2/datasets.py
+++ work2/datasets.py
@@ -7,12 +7,12 @@ class SeriesDataset:
 
     def getSyntheticData(self, size, length):
         # return sequences of length, size times. size is the number of rows.
-        return torch.randint(0, 10, (size, length))
+        return torch.FloatTensor(size, length, 1).uniform_(0, 1)
 
     def toHotVec(self, tensor):
         return torch.nn.functional.one_hot(tensor, 10).float()
 
-    def getSyntheticDataInHotVector(self, size, batch, length):
+    def getSyntheticDataInBatches(self, size, batch, length):
         # return one hot vector
-        batches = self.getSyntheticData(size, length).split(batch)
-        return list(map(self.toHotVec, batches))
+        return self.getSyntheticData(size, length).split(batch)
+
