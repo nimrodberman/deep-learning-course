4ff2c41d2592df150f5b11c7cb34b09e052c4736
naglera <anagler123@gmail.com>
2021-05-21 15:47:39 +0300

    finished

--- work2/datasets.py
+++ work2/datasets.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 class SeriesDataset:
@@ -33,20 +34,50 @@ class sp500Dataset:
     def getAllCompanyData(self, companyName):
         return self.data[(self.data.symbol == companyName)]
 
+    # def getDataForModel(self, batch_size, time=1007, show_data=False):
+    #     unnormalize_data = self.data.copy()
+    #     # convert to pandas date object
+    #     self.data['date'] = pd.to_datetime(self.data.date)
+    #     # normalize data
+    #     x = self.data.drop(['date', 'symbol'], axis=1).values
+    #
+    #     # x = self.data.drop(['date', 'symbol', 'open', 'close', 'low', 'volume'], axis=1).values
+    #
+    #     min_max_scaler = preprocessing.MinMaxScaler()
+    #     x_scaled = min_max_scaler.fit_transform(x)
+    #     self.data[['open', 'high', 'low', 'close', 'volume']] = x_scaled
+    #
+    #     # self.data['high'] = x_scaled
+    #
+    #     # sort by companies name and date
+    #     sortedCompaniesDict = dict(tuple(self.data.sort_values(['symbol', 'date']).groupby('symbol')))
+    #     sortedCompaniesList = list(sortedCompaniesDict.values())
+    #
+    #     # filter companies with less then 1007 days of data
+    #     filteredCompanies = list(filter(lambda company: company.shape[0] == 1007, sortedCompaniesList))
+    #
+    #     # keep only high
+    #     filteredCompanies = list(
+    #         map(lambda company: torch.from_numpy(company.to_numpy()[:, 3].astype(float))[:time], filteredCompanies))
+    #
+    #     data_tesor = torch.stack(filteredCompanies)
+    #
+    #     if show_data:
+    #         self.show_amazon_and_google(unnormalize_data)
+    #
+    #     m = len(data_tesor)
+    #     train = data_tesor[:(int)(3 / 4 * m)].float()
+    #     validation = data_tesor[(int)(3 / 4 * m):].float()
+    #
+    #     batchs = torch.split(train, batch_size, dim=0)
+    #     return batchs, validation
+
+
+    # normalize each sample separately
     def getDataForModel(self, batch_size, time=1007, show_data=False):
         unnormalize_data = self.data.copy()
         # convert to pandas date object
         self.data['date'] = pd.to_datetime(self.data.date)
-        # normalize data
-        x = self.data.drop(['date', 'symbol'], axis=1).values
-
-        # x = self.data.drop(['date', 'symbol', 'open', 'close', 'low', 'volume'], axis=1).values
-
-        min_max_scaler = preprocessing.MinMaxScaler()
-        x_scaled = min_max_scaler.fit_transform(x)
-        self.data[['open', 'high', 'low', 'close', 'volume']] = x_scaled
-
-        # self.data['high'] = x_scaled
 
         # sort by companies name and date
         sortedCompaniesDict = dict(tuple(self.data.sort_values(['symbol', 'date']).groupby('symbol')))
@@ -59,6 +90,9 @@ class sp500Dataset:
         filteredCompanies = list(
             map(lambda company: torch.from_numpy(company.to_numpy()[:, 3].astype(float))[:time], filteredCompanies))
 
+        normalized = list(
+            map(lambda company:  self.norm_company(company), filteredCompanies))
+
         data_tesor = torch.stack(filteredCompanies)
 
         if show_data:
@@ -68,9 +102,15 @@ class sp500Dataset:
         train = data_tesor[:(int)(3 / 4 * m)].float()
         validation = data_tesor[(int)(3 / 4 * m):].float()
 
+        # train, validation = train_test_split(data_tesor, shuffle=True, test_size=0.25)
+
         batchs = torch.split(train, batch_size, dim=0)
         return batchs, validation
 
+    def norm_company(self, company):
+        # normalize data
+        company -= company.min()
+        company /= company.max()
     def show_amazon_and_google(self, data):
         # amz_idx = 37
         # gogl_idx = 197
